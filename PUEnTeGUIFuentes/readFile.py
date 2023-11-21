1dbeb4a12802d681a1c03bbc682c0ac0a976c90d
Velyxius <dadawdasd@hotmail.com>
2023-11-21 01:12:10 -0500

    Set up project to match guidelines

---
+++ PUEnTeGUIFuentes/readFile.py
@@ -0,0 +1,52 @@
+from tkinter import filedialog
+
+
+def open_file():
+    archivo = filedialog.askopenfilename(title="Seleccionar archivo")
+    Ej, Aj, Gj, Fj, Vj, Pj_inferior, Pj_superior, Supj, Infj, Pj_inicial, Dk, Rk = (
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    )
+
+    with open(archivo) as file:
+        J = file.readline()
+        K = file.readline()
+        Ej = (file.readline()).strip().split(",")
+        Aj = (file.readline()).strip().split(",")
+        Gj = (file.readline()).strip().split(",")
+        Fj = (file.readline()).strip().split(",")
+        Vj = (file.readline()).strip().split(",")
+        Pj_inferior = (file.readline()).strip().split(",")
+        Pj_superior = (file.readline()).strip().split(",")
+        Supj = (file.readline()).strip().split(",")
+        Infj = (file.readline()).strip().split(",")
+        Pj_inicial = (file.readline()).strip().split(",")
+        Dk = (file.readline()).strip().split(",")
+        Rk = (file.readline()).strip().split(",")
+
+    return (
+        J,
+        K,
+        Ej,
+        Aj,
+        Gj,
+        Fj,
+        Vj,
+        Pj_inferior,
+        Pj_superior,
+        Supj,
+        Infj,
+        Pj_inicial,
+        Dk,
+        Rk,
+    )
---
+++ README.md
@@ -0,0 +1,26 @@
+<p align='center'>
+  <img width='200' heigth='225' src='https://user-images.githubusercontent.com/62605744/171186764-43f7aae0-81a9-4b6e-b4ce-af963564eafb.png'>
+</p>
+
+# Proyecto - Optimización Discreta
+
+- Proyecto de la asignatura Análisis y Diseño de Algoritmos 2
+- Semestre 2023-II
+- Profesores: Jesus Alexander Aranda, Juan Francisco Díaz
+
+## Autores
+
+- Johan Sebastián Tombe Campo - 2110051
+- César Alejandro Grijalba Zuñiga - 2110035
+- Marcelo Alejandro García Millán - 1941427
+- Alejandro Chará Garcia - 2010017
+
+## Instrucciones
+
+1. Hacer esto.
+
+   ```
+   py main.py
+   ```
+
+2. Hacer lo otro.
